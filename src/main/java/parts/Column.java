94a0f2fde07f3e6dcf8f564fd11da5a01b49e7fc
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-05-04 22:54:50 +0300

    add class Column

---
+++ src/main/java/parts/Column.java
@@ -0,0 +1,47 @@
+package parts;
+
+import java.awt.*;
+
+public class Column {
+    public static final int PARALLELOGRAM_SHIFT = Toolkit.getDefaultToolkit().getScreenSize().height / 50;
+    public static final int WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width / 30;
+    public static final int HEIGHT_UNIT = Toolkit.getDefaultToolkit().getScreenSize().height / 170;
+    private Graphics2D graphics;
+    private int percents;
+    private Color brightColor;
+    private Color darkColor;
+
+    public Column(Graphics graphics, int percents, Color color) {
+        this.graphics = (Graphics2D) graphics;
+        this.percents = percents;
+        this.brightColor = color.brighter();
+        this.darkColor = color.darker();
+    }
+
+    public void paint(int x, int y) {
+        //draw front side of column
+        graphics.setColor(brightColor);
+        graphics.fillRect(x, y, WIDTH, HEIGHT_UNIT * percents);
+
+        //draw top side of column
+        graphics.setColor(darkColor);
+        Polygon parallelogram = new Polygon();
+        parallelogram.addPoint(x, y);
+        parallelogram.addPoint(x + WIDTH, y);
+        parallelogram.addPoint(x + WIDTH + PARALLELOGRAM_SHIFT, y - PARALLELOGRAM_SHIFT);
+        parallelogram.addPoint(x + PARALLELOGRAM_SHIFT, y - PARALLELOGRAM_SHIFT);
+        graphics.fillPolygon(parallelogram);
+
+        //draw right side of column
+        parallelogram = new Polygon();
+        parallelogram.addPoint(x + WIDTH, y);
+        parallelogram.addPoint(x + WIDTH + PARALLELOGRAM_SHIFT, y - PARALLELOGRAM_SHIFT);
+        parallelogram.addPoint(x + WIDTH + PARALLELOGRAM_SHIFT, y - PARALLELOGRAM_SHIFT + HEIGHT_UNIT * percents);
+        parallelogram.addPoint(x + WIDTH, y + HEIGHT_UNIT * percents);
+        graphics.fillPolygon(parallelogram);
+    }
+
+    public int getHeight() {
+        return HEIGHT_UNIT * percents;
+    }
+}
